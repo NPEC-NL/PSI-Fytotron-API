58c2cb41ea21a6d1373305ccf98e1e463ae98445
Daalen <tim.vandaalen@wur.nl>
2026-02-03 11:49:56 +0100

    updated example file

--- example_implementation.py
+++ example_implementation.py
@@ -2,17 +2,23 @@
 from os import getenv
 from dotenv import load_dotenv
 
-from fytotron.fytotron import Fytotron_API
+import fytotron
 
 
 if __name__ == "__main__":
     load_dotenv()
     # Create an instance of the API class
-    api = Fytotron_API(getenv('URL'), getenv('PORT'), getenv('TOKEN'))
+    api = fytotron.FytotronApi(
+        fytotron.ApiClient(
+            fytotron.Configuration(
+                host=getenv('URL'),
+                api_key={'ApiKeyAuth': getenv('TOKEN')}
+            )
+        )
+    )
 
     # Retreive all values
-    variable_list = api.get_all_values()
-
+    variable_list = api.info()
     for var in variable_list:
         print(f"{var.name} max: {var.max}{var.unit}, \
                            min: {var.min}{var.unit}, \
@@ -20,13 +26,13 @@ if __name__ == "__main__":
 
     # Retreive current value
     variable = 'T_Set'
-    current_value = api.get_current_value(variable)
+    current_value = api.getvar(variable)
     print(f"{variable}, value: {current_value}")
 
     # Set setpoint, commented to prevent accidental execution
-    # api.set_setpoint('T_Set', 25)
+    # api.setvar('T_Set', 25)
 
     # Retreive monitoring values
-    monitor_values = api.get_monitor_values()
+    monitor_values = api.monitor()
     for var in monitor_values:
         print(f"{var.caption}: {var.state}, {var.message} ")
